0c70baa7b29934aef46694c9756f7b0956ab35d8
papoutsa20 <papoutsa20@up.edu>
2018-03-01 16:44:42 -0800

    ctors done

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -8,10 +8,29 @@ import edu.up.cs301.game.infoMsg.GameState;
 
 public class PigGameState extends GameState {
 
-    int currentPlayer;
-    int player0Score;
-    int player1Score;
-    int currentRunningScore;
-    int currentDieValue;
+    private int currentPlayer;
+    private int player0Score;
+    private int player1Score;
+    private int currentRunningScore;
+    private int currentDieValue;
+
+    public PigGameState()
+    {
+        this.currentPlayer = 0;
+        this.player0Score = 0;
+        this.player1Score = 0;
+        this.currentRunningScore = 0;
+        this.currentDieValue = 0;
+    }
+
+    public PigGameState(PigGameState copy)
+    {
+        this.currentPlayer = copy.currentPlayer;
+        this.player0Score = copy.player0Score;
+        this.player1Score = copy.player1Score;
+        this.currentRunningScore = copy.currentRunningScore;
+        this.currentDieValue = copy.currentDieValue;
+
+    }
 
 }
