5d64ae4010b2fa81099783a0ddb715d5a87e4b71
mfredenberg <fredenbe20@up.edu>
2018-03-01 16:46:41 -0800

    Made PigGameState + variables

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -14,4 +14,27 @@ public class PigGameState extends GameState {
     int currentRunningScore;
     int currentDieValue;
 
+    public PigGameState() {
+
+    }
+
+    public PigGameState(PigGameState original){
+
+    }
+    //getters for all variables
+    public int getCurrentPlayer(){return currentPlayer;}
+    public int getPlayer0Score(){return player0Score;}
+    public int getPlayer1Score(){return player1Score;}
+    public int getCurrentRunningScore(){return currentRunningScore;}
+    public int getCurrentDieValue(){return currentDieValue;}
+
+    public void setCurrentPlayer(int newPlayer){this.currentPlayer = newPlayer;}
+
+    public void setPlayer0Score(int player0Score){this.player0Score = player0Score;}
+
+    public void setPlayer1Score(int player1Score){this.player1Score = player1Score;}
+
+    public void setCurrentRunningScore(int currentRunningScore){this.currentRunningScore = currentRunningScore;}
+
+    public void setCurrentDieValue(int currentDieValue){this.currentDieValue = currentDieValue;}
 }
