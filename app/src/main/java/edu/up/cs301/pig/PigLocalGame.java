d731e13187580f84fb4eec76aef5753b59799bed
mfredenberg <fredenbe20@up.edu>
2018-03-01 17:06:20 -0800

    wrote sendUpdatedStateTo() and checkIfGameOver() methods

--- app/src/main/java/edu/up/cs301/pig/PigLocalGame.java
+++ app/src/main/java/edu/up/cs301/pig/PigLocalGame.java
@@ -50,6 +50,8 @@ public class PigLocalGame extends LocalGame {
      */
     @Override
     protected void sendUpdatedStateTo(GamePlayer p) {
+        PigGameState playerPigGameState = new PigGameState(pig);
+        p.sendInfo(playerPigGameState);
         //TODO  You will implement this method
     }//sendUpdatedSate
 
@@ -62,6 +64,12 @@ public class PigLocalGame extends LocalGame {
      */
     @Override
     protected String checkIfGameOver() {
+        if(pig.getPlayer0Score() >= 50){
+            return "Player 0 Wins";
+        }
+        else if(pig.getPlayer1Score() >= 50){
+            return "Player 1 Wins";
+        }
         //TODO  You will implement this method
         return null;
     }
